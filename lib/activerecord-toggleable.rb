d4f616333f1573fd77d057d7b014cf061c32c9de
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-04 22:02:44 +0200

    Update gemspec

--- lib/activerecord-toggleable.rb
+++ lib/activerecord-toggleable.rb
@@ -1,4 +1,8 @@
+require 'active_support/all'
 require 'active_record'
+require 'attribute-defaults'
+require 'activerecord-traits'
+
 require 'activerecord-toggleable/migration'
 require 'activerecord-toggleable/extension'
 require 'activerecord-toggleable/essay' if defined?(Essay)
