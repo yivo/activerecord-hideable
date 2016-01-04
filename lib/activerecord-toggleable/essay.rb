89315f83ec324cb7dff43776f8e1c07ea35f05b2
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-04 19:18:26 +0200

    Update essay

--- lib/activerecord-toggleable/essay.rb
+++ lib/activerecord-toggleable/essay.rb
@@ -3,15 +3,23 @@ module Essay
     def hideable?
       model_class.hideable_options.any?
     end
+
+    serialize do
+      { is_hideable: hideable? }
+    end
   end
 
-  class AttributeRoles
+  class AttributeFeatures
     def toggle?
       model_class.toggles_options.key?(attribute_name)
     end
 
     def hideable_toggle?
       model_class.hideable_options[:toggle] == attribute_name
+    end
+
+    serialize do
+      { is_toggle: toggle?, is_hideable_toggle: hideable_toggle? }
     end
   end
 end
